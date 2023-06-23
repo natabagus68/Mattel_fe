a543d802db102cc5d219506981dd3772df73938d
Brian-Kraosio <briansayudha@gmail.com>
2023-06-23 09:31:50 +0700

    remove access

--- src/features/admin/adminLayout/AdminLayout.jsx
+++ src/features/admin/adminLayout/AdminLayout.jsx
@@ -28,26 +28,26 @@ export const AdminLayout = () => {
   }
   const { data, isSuccess } = useMeQuery();
 
-  useLayoutEffect(() => {
-    if (isSuccess) {
-      dispatch(
-        setPermissions({
-          permission: data.data.positions[0].permissions.map((el) =>
-            el.name.toLowerCase()
-          ),
-        })
-      );
-      const permissions = data.data.positions[0].permissions.map((el) =>
-        el.name.toLowerCase().replace("#", "")
-      );
-      const prefix = pathname.split("/")[2];
-      const permit = permissions.includes(prefix);
-      if (!permit) {
-        navigate(`${config.pathPrefix}404`);
-      }
-    }
-    return () => {};
-  }, [pathname, isSuccess]);
+  // useLayoutEffect(() => {
+  //   if (isSuccess) {
+  //     dispatch(
+  //       setPermissions({
+  //         permission: data.data.positions[0].permissions.map((el) =>
+  //           el.name.toLowerCase()
+  //         ),
+  //       })
+  //     );
+  //     const permissions = data.data.positions[0].permissions.map((el) =>
+  //       el.name.toLowerCase().replace("#", "")
+  //     );
+  //     const prefix = pathname.split("/")[2];
+  //     const permit = permissions.includes(prefix);
+  //     if (!permit) {
+  //       navigate(`${config.pathPrefix}404`);
+  //     }
+  //   }
+  //   return () => {};
+  // }, [pathname, isSuccess]);
 
   // if (isError || !auth?.data) return <Navigate to={`login`} />;
   if (loading) {
@@ -60,18 +60,16 @@ export const AdminLayout = () => {
         {/*<ConfirmationModal />*/}
         <div className="w-full">
           <div
-            className={`${
-              navOpen == null || navOpen === true
-                ? "ml-0 md:ml-[256px]"
-                : "ml-0 md:ml-0"
-            }`}
+            className={`${navOpen == null || navOpen === true
+              ? "ml-0 md:ml-[256px]"
+              : "ml-0 md:ml-0"
+              }`}
           >
             <div
-              className={`fixed top-0 flex bg-white-lightest z-10 shadow-[0_1px_20px_rgba(0,0,0,0.17)] ${
-                navOpen == null || navOpen === true
-                  ? "w-[calc(100%-256px)]"
-                  : "w-full"
-              } h-[84px]`}
+              className={`fixed top-0 flex bg-white-lightest z-10 shadow-[0_1px_20px_rgba(0,0,0,0.17)] ${navOpen == null || navOpen === true
+                ? "w-[calc(100%-256px)]"
+                : "w-full"
+                } h-[84px]`}
             >
               <div className="py-[15px] px-[48px] flex-1 flex">
                 <div className="flex-1 flex items-center">
@@ -92,9 +90,8 @@ export const AdminLayout = () => {
           <div className="relative z-0">
             <SideBar />
             <div
-              className={`${
-                (navOpen == null || navOpen === true) && `md:ml-[274px]`
-              } transition-[margin] mt-[78px] py-6 md:py-[37px] px-2 md:px-[48px] flex-1 overflow-auto`}
+              className={`${(navOpen == null || navOpen === true) && `md:ml-[274px]`
+                } transition-[margin] mt-[78px] py-6 md:py-[37px] px-2 md:px-[48px] flex-1 overflow-auto`}
             >
               <Outlet />
             </div>
--- src/features/admin/adminLayout/SideBar.jsx
+++ src/features/admin/adminLayout/SideBar.jsx
@@ -17,70 +17,58 @@ export const SideBar = () => {
   return (
     <>
       <div
-        className={`${
-          navOpen == null && `w-0 pl-0 md:w-[256px] md:pl-[32px]`
-        } ${
-          navOpen === true ? `w-[256px] pl-[32px]` : `w-0 pl-0`
-        } fixed top-[0px] left-0 overflow-x-hidden overflow-y-auto transition-[width_padding] flex flex-col gap-4 bg-black-base pt-[19px] h-full min-h-[calc(100vh_-_78px)] pb-12`}
+        className={`${navOpen == null && `w-0 pl-0 md:w-[256px] md:pl-[32px]`
+          } ${navOpen === true ? `w-[256px] pl-[32px]` : `w-0 pl-0`
+          } fixed top-[0px] left-0 overflow-x-hidden overflow-y-auto transition-[width_padding] flex flex-col gap-4 bg-black-base pt-[19px] h-full min-h-[calc(100vh_-_78px)] pb-12`}
       >
         {/*App Logo*/}
         <div className="flex items-center mb-[64px]">
           <img className="mr-[13.74px]" src={appLogo} alt="" />
           <AppNameText />
         </div>
-        {permission.includes("#dashboard") && (
+        <NavItem
+          label={`Dashboard`}
+          icon={<DashboardIcon className="mr-3 -mb-1" />}
+        >
+          <NavItem to={`dashboard/general`} label={`General`} />
+          <NavItem to={`dashboard/man-power`} label={`Man Power`} />
           <NavItem
-            label={`Dashboard`}
-            icon={<DashboardIcon className="mr-3 -mb-1" />}
-          >
-            <NavItem to={`dashboard/general`} label={`General`} />
-            <NavItem to={`dashboard/man-power`} label={`Man Power`} />
-            <NavItem
-              to={`dashboard/machine-problem`}
-              label={`Machine Problem`}
-            />
-          </NavItem>
-        )}
-        {permission.includes("#monitoring") && (
-          <NavItem
-            label={`Monitoring`}
-            icon={<MonitorIcon className="mr-3 -mb-1" />}
-          >
-            <NavItem label={`Line`} to={`monitoring/line`} />
-            <NavItem label={`Layout`} to={`monitoring/layout`} />
-            <NavItem label={`Mechanic`} to={`monitoring/mechanic`} />
-          </NavItem>
-        )}
-        {permission.includes("#report") && (
-          <NavItem
-            to={`report`}
-            label={`Report`}
-            icon={<ChartIcon className="mr-3 -mb-1" />}
+            to={`dashboard/machine-problem`}
+            label={`Machine Problem`}
           />
-        )}
+        </NavItem>
+        <NavItem
+          label={`Monitoring`}
+          icon={<MonitorIcon className="mr-3 -mb-1" />}
+        >
+          <NavItem label={`Line`} to={`monitoring/line`} />
+          <NavItem label={`Layout`} to={`monitoring/layout`} />
+          <NavItem label={`Mechanic`} to={`monitoring/mechanic`} />
+        </NavItem>
+        <NavItem
+          to={`report`}
+          label={`Report`}
+          icon={<ChartIcon className="mr-3 -mb-1" />}
+        />
 
-        {permission.includes("#master") && (
+        <NavItem
+          label={`Master`}
+          icon={<MasterDataIcon className="mr-3 -mb-1" />}
+        >
+          <NavItem to={`master/line`} label={`Line`} />
+          <NavItem to={`master/line-group`} label={`Line Group`} />
+          <NavItem to={`master/line-device`} label={`Line Device`} />
+          <NavItem to={`master/part`} label={`Machine Part`} />
           <NavItem
-            label={`Master`}
-            icon={<MasterDataIcon className="mr-3 -mb-1" />}
-          >
-            <NavItem to={`master/line`} label={`Line`} />
-            <NavItem to={`master/line-group`} label={`Line Group`} />
-            <NavItem to={`master/line-device`} label={`Line Device`} />
-            <NavItem to={`master/part`} label={`Machine Part`} />
-            <NavItem
-              to={`master/machine-category`}
-              label={`Machine Category`}
-            />
-            <NavItem to={`master/machine`} label={`Machine`} />
-          </NavItem>
-        )}
-        {permission.includes("#user") && (
-          <NavItem label={`User`} icon={<UsersIcon className="mr-3 -mb-1" />}>
-            <NavItem to={`user/account`} label={`Account`} />
-            <NavItem to={`user/access`} label={`Access`} />
-          </NavItem>
-        )}
+            to={`master/machine-category`}
+            label={`Machine Category`}
+          />
+          <NavItem to={`master/machine`} label={`Machine`} />
+        </NavItem>
+        <NavItem label={`User`} icon={<UsersIcon className="mr-3 -mb-1" />}>
+          <NavItem to={`user/account`} label={`Account`} />
+          <NavItem to={`user/access`} label={`Access`} />
+        </NavItem>
       </div>
     </>
   );
